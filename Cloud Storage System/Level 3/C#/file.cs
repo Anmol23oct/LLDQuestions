27d66a959c7d7511ab45072eec41f865f57b925a
Anmol Sharma <anmol23oct@ymail.com>
2025-05-11 14:09:53 -0500

    Create file.cs

---
+++ Cloud Storage System/Level 3/C#/file.cs
@@ -0,0 +1,12 @@
+// File.cs
+public class File
+{
+    public string Path { get; }
+    public int Size { get; }
+
+    public File(string path, int size)
+    {
+        Path = path;
+        Size = size;
+    }
+}
