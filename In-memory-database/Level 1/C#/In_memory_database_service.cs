d9463aec121ebea3ad8cc6038293b5521e2db2ff
Anmol Sharma <anmol23oct@ymail.com>
2025-05-13 07:48:26 -0500

    Create In_memory_database_service.cs

---
+++ In-memory-database/Level 1/C#/In_memory_database_service.cs
@@ -0,0 +1,30 @@
+// InMemoryDatabaseService.cs
+
+public class InMemoryDatabaseService
+{
+    private readonly InMemoryRepository _repository;
+
+    public InMemoryDatabaseService(InMemoryRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public string Set(string key, string field, string value)
+    {
+        var record = _repository.GetOrCreate(key);
+        record.SetField(field, value);
+        return "";
+    }
+
+    public string Get(string key, string field)
+    {
+        var record = _repository.GetRecord(key);
+        return record?.GetField(field) ?? "";
+    }
+
+    public string Delete(string key, string field)
+    {
+        var record = _repository.GetRecord(key);
+        return record != null && record.DeleteField(field) ? "true" : "false";
+    }
+}
